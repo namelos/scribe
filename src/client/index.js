b5fcff10299b834ca06c116d5dc883366e4c1d22
Yifan Wang <namelos@outlook.com>
2016-11-19 16:03:13 +0800

    change misused component name

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
-import Counter from '../common/component/Counter'
+import App from '../common/component/TodoApp'
 
 const client = new ApolloClient()
 
 render(
   <ApolloProvider client={client}>
-    <Counter />
+    <App />
   </ApolloProvider>, document.querySelector('#app'))
